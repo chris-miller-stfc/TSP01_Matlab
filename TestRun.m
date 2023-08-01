7193ead76a4c842871dd004ac4d1b687628e6f67
Chris Miller <chris.miller@stfc.ac.uk>
2023-08-01 17:26:14 +0100

    Added comments

--- TestRun.m
+++ TestRun.m
@@ -1,8 +1,19 @@
  addpath('TSP01');
 
+ %Initlise a temperature sensor at index 0.
+ %If multiple TSP01s are connected you can use the index to select the
+ %approiate device.
  TemperatureSensor = TSP01(0);
+
+ %Read internal temperature (DegC)
  Internal_Temp = TemperatureSensor.GetInternalTemperature();
+ 
+ %Read temperature from channel 1 and 2 (DegC)
  Ch1_Temp = TemperatureSensor.GetCh1Temperature();
  Ch2_Temp = TemperatureSensor.GetCh2Temperature();
+ 
+ %Measure humidity in %
  Humidity = TemperatureSensor.GetHumidity();
+ 
+ %Close temperature sensor
  TemperatureSensor.close();
